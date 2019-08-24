6d58450be247bf70d2d2e83f0534fbdf0aa79891
Dinosaur Burger <28460277+Zebraslive@users.noreply.github.com>
2019-08-24 12:27:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  io.emit('chat message', socket.client.conn.server.clientsCount);
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
