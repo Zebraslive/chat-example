00acb5282f79f764d4c5a29e662ea55cb059af42
Dinosaur Burger <28460277+Zebraslive@users.noreply.github.com>
2019-08-24 15:59:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,16 +15,7 @@ io.on('connection', function(socket){
     
         socket.nickname = msg.username;
    users[msg.username] = socket.id;
-     
-         for (i = 0; i < users.length; i++) {
-  if (msg.username === users[i]) {
      users[msg.username].push(socket.id);
-  } else {
-     socket.nickname = msg.username;
-   users[msg.username] = socket.id;
-  }
-
-     }
     
     
      vuur = socket.client.conn.server.clientsCount;
