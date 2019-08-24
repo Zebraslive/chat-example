e5cfe856b8645a749ab3bba7159830956c678014
Dinosaur Burger <28460277+Zebraslive@users.noreply.github.com>
2019-08-24 15:30:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ app.get('/', function(req, res){
 var vuur = 0;
 var users = [];
 io.on('connection', function(socket){
-  
+    if (users.length <= 0) {
+        socket.nickname = socket.username;
+   users[socket.nickname] = socket.id;
+     }
    socket.on('visit', function(msg){
      var i;
-     if (users.length <= 0) {
-        socket.nickname = msg.username;
-   users[socket.nickname] = socket.id;
-     } else {
+    
          for (i = 0; i < users.length; i++) {
   if (msg.username === users[i]) {
      users[msg.username].push(socket.id);
@@ -23,7 +23,7 @@ io.on('connection', function(socket){
      socket.nickname = msg.username;
    users[socket.nickname] = socket.id;
   }
-}
+
      }
     io.emit('tik', users.length+"bha");
     
