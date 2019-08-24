1ea21f98f8e0c2ce42cfdc1073e9f212f14decda
Dinosaur Burger <28460277+Zebraslive@users.noreply.github.com>
2019-08-24 12:22:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -9,7 +9,7 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg+port);
+    io.emit('chat message', msg);
   });
 });
 
