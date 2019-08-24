9f81aeefff40ce9c26c392b73420f177b7266788
Dinosaur Burger <28460277+Zebraslive@users.noreply.github.com>
2019-08-24 15:46:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,27 +7,28 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 var vuur = 0;
-var users = {};
+const users = [];
 io.on('connection', function(socket){
-   var soiffi = socket.username;
-      users[socket.username] = socket;
-     
+    if (users.length <= 0) {
+        socket.nickname = socket.username;
+   users[socket.nickname] = socket.id;
+     }
    socket.on('visit', function(msg){
      var i;
     
          for (i = 0; i < users.length; i++) {
   if (msg.username === users[i]) {
-     users[msg.username].push(socket);
+     users[msg.username].push(socket.id);
   } else {
-  
-   users[msg.username] = socket;
+     socket.nickname = msg.username;
+   users[socket.nickname] = socket.id;
   }
 
      }
-   
+    io.emit('tik', users.length+"bha");
     
      vuur = socket.client.conn.server.clientsCount;
-     io.emit('visit', {total: vuur, uniq: ''+users.length+'', userx: soiffi});
+     io.emit('visit', {total: vuur, uniq: users.length});
    });
   socket.on('disconnect', function () {
     vuur = socket.client.conn.server.clientsCount;
