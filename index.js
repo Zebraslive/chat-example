818ea54a1caa7bfe8c9b3774a68724b30e88162d
Dinosaur Burger <28460277+Zebraslive@users.noreply.github.com>
2019-08-25 00:23:31 -0400

    updates

--- index.js
+++ index.js
@@ -9,11 +9,14 @@ app.get('/', function(req, res){
 var vuur = 0;
 const users = {};
 const connsx = {};
+const watching = {};
 io.on('connection', function(socket){
 
    socket.on('visit', function(msg){
+     socket.user_type = msg.type;
  socket.username = msg.username;
  users[msg.username] = socket.id;
+ users[msg.username]['type'] = socket.user_type;
  connsx[socket.id] = msg.username;
  var actualx = Object.keys(users).length;
      vuur = socket.client.conn.server.clientsCount;
@@ -27,10 +30,62 @@ delete users[userixk];
  if (vuur == 1) {
    actualx = 1;
  }
-    io.emit('visit', {total: vuur, uniq: actualx});
+    io.emit('leftserver', {total: vuur, uniq: actualx});
+  });
+  socket.on('click Episode', function(msg){
+     users[msg.sid] = msg.tot;
+  io.emit('click Episode', {total: msg.tot, sid: msg.sid});
   });
 });
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+// jsonObject = JSON.stringify({
+//     "message" : "The web of things is approaching, let do some tests to be ready!",
+//     "name" : "Test message posted with node.js",
+//     "picture" : "http://youscada.com/wp-content/uploads/2012/05/logo2.png",
+//     "actions" : [ {
+//         "name" : "youSCADA",
+//         "link" : "http://www.youscada.com"
+//     } ]
+// });
+//
+// // prepare the header
+// var postheaders = {
+//     'Content-Type' : 'application/json',
+//     'Content-Length' : Buffer.byteLength(jsonObject, 'utf8')
+// };
+//
+// // the post options
+// var optionspost = {
+//     host : 'graph.facebook.com',
+//     port : 443,
+//     path : '/youscada/feed?access_token=your_api_key',
+//     method : 'POST',
+//     headers : postheaders
+// };
+//
+// console.info('Options prepared:');
+// console.info(optionspost);
+// console.info('Do the POST call');
+//
+// // do the POST call
+// var reqPost = https.request(optionspost, function(res) {
+//     console.log("statusCode: ", res.statusCode);
+//     // uncomment it for header details
+// //  console.log("headers: ", res.headers);
+// 
+//     res.on('data', function(d) {
+//         console.info('POST result:\n');
+//         process.stdout.write(d);
+//         console.info('\n\nPOST completed');
+//     });
+// });
+//
+// // write the json data
+// reqPost.write(jsonObject);
+// reqPost.end();
+// reqPost.on('error', function(e) {
+//     console.error(e);
+// });
