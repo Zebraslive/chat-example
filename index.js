febc3daacdac91b180923842cdfef9a66eac6fb4
Dinosaur Burger <28460277+Zebraslive@users.noreply.github.com>
2019-09-07 15:09:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 const fs = require('fs');
 	const path = require('path');
-	const pathToFiles = 'node_modules/node-chat-js/';
+	const pathToFiles = '';
 	const bodyParser = require('body-parser');
 	// create a session with a random string as secret
 	const session = require('express-session')({
@@ -12,6 +12,10 @@ const fs = require('fs');
 		resave : true,
 		saveUninitialized : true
 	});
+  const sharedSession = require('express-socket.io-session');
+	const bcrypt = require('bcrypt');
+	// difficulty of the hash function
+	const saltRounds = 10;
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -42,6 +46,7 @@ delete users[userixk];
    actualx = 1;
  }
     io.emit('leftserver', {total: vuur, uniq: actualx, user:userixk});
+    		socket.broadcast.emit('updateClientNumber', { clientNumber : Object.keys(io.sockets.connected).length });
   });
   socket.on('click Episode', function(msg){
 if (msg.sid in watching) {
@@ -94,6 +99,87 @@ io.emit('click Episode', {total_watching: msg.tot, sid: msg.sid, allU:actfsf});
 }
 
   });
+  // open log in append mode
+		let logWriter = fs.createWriteStream(`${ pathToFiles }log`, { flags : 'a' });
+		// emit and update clients number
+		socket.emit('updateClientNumber', { clientNumber : Object.keys(io.sockets.connected).length });
+		socket.broadcast.emit('updateClientNumber', { clientNumber : Object.keys(io.sockets.connected).length });
+
+		// banClient function
+		// (case detected @IP banned at connexion (no arg)) : @IP is not allowed to create a socket conn => disconnect
+		// (case asked by admin on connected client(ip arg)) : for all socketId owned by @IP banned => disconnect
+		const banClient = (ip) => {
+			let bannedObject = {
+				message : 'It looks like you did something wrong, you are banned from the chat',
+				time : dateUtil.time()
+			}
+			if (!ip) {
+				socket.emit('banned', bannedObject);
+				socket.disconnect();
+			} else {
+				for (let client of clients) {
+					if (client.ip === ip) {
+						io.sockets.to(client.id).emit('banned', bannedObject);
+						socket.disconnect();
+					}
+				}
+			}
+		}
+
+		// get client @IP, store in const clients[] and log
+		let handshake = socket.handshake;
+		let ipClient = handshake.address;
+		clients.push({ ip : ipClient, id : socket.id });
+		logWriter.write(`INFO_CONN -- ${ dateUtil.time() } -- ${ ipClient }\n`);
+
+		// if ipClient is in bannedAddresses => disconnect from chat
+		let bannedAddresses = fs.readFileSync(`${ pathToFiles }banned-addresses`, 'utf-8').split(/\r?\n/);
+		if (bannedAddresses.includes(ipClient)) {
+			banClient();
+		}
+
+		// message from client => broadcast to all clients
+		socket.on('message', (data) => {
+			logWriter.write(`NEW_MSG -- ${ dateUtil.fullTime() } -- ${ ipClient } -- ${ data.name } -- ${ data.message }\n`);
+			socket.broadcast.emit('message', { name : data.name, message : data.message, time : dateUtil.time() });
+			socket.broadcast.emit('messageForAdmin', { name : data.name, message : data.message, ipClient : ipClient, time : dateUtil.time() });
+		});
+
+		// message from admin => broadcast to all clients with style
+		socket.on('messageFromAdmin', (data) => {
+			if(socket.handshake.session.isAdmin) {
+				logWriter.write(`ADMIN_MSG -- ${ dateUtil.fullTime() } name : ${ data.name } MESSAGE : ${ data.message }\n`);
+				socket.broadcast.emit('messageFromAdmin', { name : data.name, message : data.message, time : dateUtil.time() });
+			}
+		});
+
+		// ban this IP on request from an admin
+		// server log, write to banned-addresses, call banClient(), push to globalBannedAddresses
+		socket.on('banIp', (ip) => {
+			if(socket.handshake.session.isAdmin) {
+				logWriter.write(`ADMIN_BAN -- ${ dateUtil.fullTime() } -- ${ ip }\n`);
+				let banWriter = fs.createWriteStream(`${ pathToFiles }banned-addresses`, { flags : 'a' });
+				banWriter.write(`\n${ ip }`);
+				banWriter.end();
+				banClient(ip);
+				globalBannedAddresses.push(ip);
+			}
+		});
+
+		// add administrator
+		socket.on('addAdmin', (data) => {
+			if(socket.handshake.session.isAdmin || setupMode) {
+				bcrypt.hash(data.password, saltRounds, (err, hash) => {
+					let usersWriter = fs.createWriteStream(`${ pathToFiles }users`, { flags : 'a' });
+					usersWriter.write(`\n${ data.name } ${ hash }`);
+					usersWriter.end();
+				});
+				globalUsersFormated.push(data.name);
+			}
+		});
+
+		// update client number on disconnect
+
 });
 
 http.listen(port, function(){
